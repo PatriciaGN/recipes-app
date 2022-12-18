5f248ceffbef6a83d01ee4b6278f800b22a4b874
PatriciaGN <pgarcia16@alu.ucam.edu>
2022-12-18 18:27:51 +0000

    Add recipe model

---
+++ API/models/recipe.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, Sequelize) => {
+    const Recipe = sequelize.define("recipes", {
+      name: {
+        type: Sequelize.STRING
+      },
+      ingredients: {
+        type: Sequelize.STRING
+      },
+      quantity: {
+        type: Sequelize.STRING
+      },
+      isDeleted: {
+        type: Sequelize.BOOLEAN
+      }
+    });
+    Recipes.belongsToMany(Ingredient, { through: 'RecipeIngredients'})
+    return Recipe;
+  };
