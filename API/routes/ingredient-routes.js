45a65e4a19e32be0ac1fa3b38b2bfea792d25e53
PatriciaGN <pgarcia16@alu.ucam.edu>
2022-12-18 18:56:51 +0000

    Add ingredient routes

---
+++ API/routes/ingredient-routes.js
@@ -0,0 +1,25 @@
+const { jwtAuth } = require("../middleware");
+const productServices = require("../services/ingredient-services.js");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Headers", "Authorization", "Origin, Content-Type, Accept");
+    next();
+  });
+
+  // add an ingredient 
+  app.post("/api/auth/addingredient", [jwtAuth.verifyToken], ingredientServices.create);
+
+  // find all ingredients
+  app.get("/api/auth/getingredients", [jwtAuth.verifyToken], ingredientServices.findAll);
+
+  // find ingredient by ingredient id
+  app.post("/api/auth/getingredientbyid", [jwtAuth.verifyToken], ingredientServices.findOne);
+
+  // update ingredient by ingredient id
+  app.post("/api/auth/updateingredient", [jwtAuth.verifyToken], ingredientServices.update);
+
+  // delete ingredient by ingredient id
+  app.post("/api/auth/deleteingredient", [jwtAuth.verifyToken], ingredientServices.delete);
+
+};
